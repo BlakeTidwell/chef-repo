884585c8c30ed45cc10f0d8b79c3067a00468415
Blake Tidwell <batidwell@gmail.com>
2014-03-26 21:29:44 -0700

    Import BlakeTidwell/cookbook-wp-cli version 9e24bdc2f8bb397e4323fc9b450a14c0b53f37db

---
+++ cookbooks/wp-cli/definitions/wp_cli_core.rb
@@ -0,0 +1,27 @@
+define :wp_core_install do
+  missing = []
+  [:url, :title, :admin_user, :admin_password, :admin_email, :dir].each do |required|
+    if params[required].nil?
+      missing.push(required.to_s)
+    end
+  end
+  raise "WP Core install is missing required parameter(s): #{missing.join(', ')}" if !missing.empty?
+
+  execute "install site" do
+    cwd params[:dir]
+    command "wp core install --url=#{params[:url]} " +
+            "--title='#{params[:title]}' --admin_user=#{params[:admin_user]} " +
+            "--admin_password=#{params[:admin_password]} --admin_email=#{params[:admin_email]}"
+    not_if "mysql -u root -D #{node['wordpress']['db']['database']} " +
+           "-p#{node['mysql']['server_root_password']} " +
+           "-e 'DESCRIBE #{node['wordpress']['db']['prefix']}users;'"
+  end
+end
+
+define :wp_plugin_activate do
+  execute "activate plugin" do
+    cwd node['wp_scaffold']['dir']
+    command "wp plugin activate #{params[:name]}"
+    # not_if do FileTest.directory?("#{node['wp_scaffold']['dir']}/wp-content/plugins/#{params[:name]}") end
+  end
+end
